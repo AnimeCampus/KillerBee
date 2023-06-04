2ae04ec5ab9236ff7ec10cd6d920ff84c578f27b
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-06-04 08:32:57 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -14,8 +14,8 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "19099900" # integer value, dont use ""
-    API_HASH = "2b445de78e5baf012a0793e60bd4fbf5"
+    API_ID = "16743442" # integer value, dont use ""
+    API_HASH = "12bbd720f4097ba7713c5e40a11dfd2a"
     TOKEN = "6062250856:AAEJhVd4Xa3Fvn8M1OqCGldzhtmP252liwQ"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = "6198858059" # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "SexyNano"
